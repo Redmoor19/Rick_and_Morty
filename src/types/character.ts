2ac64ec5c576f530c7ea602d79ce00a8a338d285
Danila Mandrabura <danilamandrabura@gmail.com>
2024-07-17 15:21:17 +0300

    Add basic types from API

---
+++ src/types/character.ts
@@ -0,0 +1,19 @@
+export type CharLocation = {
+    origin: string;
+    url: string;
+}
+
+export type Character = {
+    id: number;
+    name: string;
+    status: string;
+    species: string;
+    type: string;
+    gender: string;
+    origin: CharLocation;
+    location: CharLocation;
+    image: string;
+    episode: string[];
+    url: string;
+    created: string;
+}
